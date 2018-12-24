d72401c7387a230fed566815448aa41502aa3842
Acta <>
2018-12-24 18:14:09 +0900

    added utils unit tests

---
+++ src/Sections/__tests__/Utils_TESTS/sumArrayValues.test.js
@@ -0,0 +1,12 @@
+import sumArrayValues from "../../../Utils/sumArrayValues";
+
+const parameter1 = [0.08490936, 0.14749678, 0.13555839, 0.1120683, 0.30110465, 0.22890879, 0.33954037, 0.26587213, 0.00002864];
+
+const testValue1 = 1.61548741;
+
+test("sum array values", () => {
+    let value1 = sumArrayValues(parameter1);
+    expect(value1).toEqual(testValue1);
+});
+
+
