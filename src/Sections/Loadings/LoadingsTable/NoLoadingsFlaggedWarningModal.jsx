a280be6624ed9f1f430074f41b34361ed1af8d3c
Shawn Banasick <banasick@mail.kobe-c.ac.jp>
2019-01-04 15:04:17 +0900

    fixed button color when no flagged factor modal fires

--- src/Sections/Loadings/LoadingsTable/NoLoadingsFlaggedWarningModal.jsx
+++ src/Sections/Loadings/LoadingsTable/NoLoadingsFlaggedWarningModal.jsx
@@ -3,19 +3,22 @@ import { view } from "react-easy-state";
 import { Button, Header, Modal } from "semantic-ui-react";
 import state from "../../../store";
 
+const handleClose = () => {
+  state.setState({
+    showNoLoadingsFlaggedWarningModal: false,
+    showOutputFactorSelection: false,
+    showFactorCorrelationsTable: false,
+    showStandardErrorsDifferences: false,
+    showFactorCharacteristicsTable: false,
+    showDownloadOutputButtons: false,
+    shouldDisplayFactorVizOptions: false,
+    displayFactorVisualizations: false,
+    sendDataToOutputButtonColor: "#d6dbe0",
+  });
+}
+
+
 class NoLoadingsFlaggedWarningModal extends Component {
-  handleClose() {
-    state.setState({
-      showNoLoadingsFlaggedWarningModal: false,
-      showOutputFactorSelection: false,
-      showFactorCorrelationsTable: false,
-      showStandardErrorsDifferences: false,
-      showFactorCharacteristicsTable: false,
-      showDownloadOutputButtons: false,
-      shouldDisplayFactorVizOptions: false,
-      displayFactorVisualizations: false
-    });
-  }
 
   render() {
     const showNoLoadingsFlaggedWarningModal = state.getState(
@@ -24,18 +27,18 @@ class NoLoadingsFlaggedWarningModal extends Component {
     const factorsWithoutLoading = state.getState("factorsWithoutLoading");
     if (showNoLoadingsFlaggedWarningModal) {
       return (
-        <Modal open={ showNoLoadingsFlaggedWarningModal } onClose={ this.handleClose }>
+        <Modal open={ showNoLoadingsFlaggedWarningModal } onClose={ handleClose }>
           <Header content="Error Checking" />
           <Modal.Content>
             <span style={ { fontSize: 30, display: "block" } }>
-                      A factor without a flagged loading was selected.
-                    </span>
+                              A factor without a flagged loading was selected.
+                            </span>
             <span style={ { fontSize: 22, marginTop: 20, display: "block" } }>
-                      Problem factors: { factorsWithoutLoading }
-                    </span>
+                              Problem factors: { factorsWithoutLoading }
+                            </span>
           </Modal.Content>
           <Modal.Actions>
-            <Button id="noLoadingsFlaggedModalGotItButton" color="green" style={ { margin: 15 } } floated="right" onClick={ this.handleClose } inverted>
+            <Button id="noLoadingsFlaggedModalGotItButton" color="green" style={ { margin: 15 } } floated="right" onClick={ handleClose } inverted>
               Got it
             </Button>
           </Modal.Actions>
