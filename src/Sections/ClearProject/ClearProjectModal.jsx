f69224ec835b61c00362725e7b98a8b0c7399bf0
Shawn Banasick <banasick@mail.kobe-c.ac.jp>
2019-01-05 11:26:52 +0900

    added project cleared success toast

--- src/Sections/ClearProject/ClearProjectModal.jsx
+++ src/Sections/ClearProject/ClearProjectModal.jsx
@@ -2,54 +2,68 @@ import styled from "styled-components";
 import React, { Component } from "react";
 import { view, store } from "react-easy-state";
 import { Button, Header, Modal } from "semantic-ui-react";
+import { ToastContainer, toast, Zoom } from "react-toastify";
 import state from "../../store";
 import initialState from "../../initialState";
 
+
 const localStore = store({
-    modalOpen: false
+  modalOpen: false
 });
 
 const handleOpen = () => {
-    localStore.modalOpen = true;
+  localStore.modalOpen = true;
 };
 
 const handleClose = () => {
-    localStore.modalOpen = false;
+  localStore.modalOpen = false;
 };
 
 const clearAnalysis = () => {
-    const initialStateValues = initialState();
-    state.setState(initialStateValues);
-    localStore.modalOpen = false;
+  const initialStateValues = initialState();
+  state.setState(initialStateValues);
+  localStore.modalOpen = false;
+  notify();
 };
 
+
+function notify() {
+  toast.success("Project Cleared");
+// state.setState({
+//   notifyDataUploadSuccess: false
+// });
+}
+
 class ClearProjectModal extends Component {
-    render() {
-        return (
-            <Modal dimmer={ "blurring" } trigger={ <ClearProjectButton onClick={ handleOpen } size="large">
-                                         Clear Project
-                                       </ClearProjectButton> } open={ localStore.modalOpen } className="wrapper1" onClose={ this.handleClose } basic size={ "small" }>
-              <Header content="Clear Project" />
-              <Modal.Content>
-                <h2>
-                                          This will remove all data and analysis, and cannot be reversed.
-                                        </h2>
-                <h2> Are you sure you want to clear the current project?</h2>
-              </Modal.Content>
-              <Modal.Actions>
-                <div style={ { display: "flex" } }>
-                  <Button size={ "big" } style={ { alignSelf: "flexStart" } } color="green" onClick={ handleClose } inverted>
-                    No, Go back.
-                  </Button>
-                  <Button id="resetAnalysisModalGotItButton" size={ "big" } style={ { alignSelf: "flexEnd", marginLeft: 220 } } color="red" onClick={ clearAnalysis } inverted>
-                    Yes, delete the
-                    <br /> data and analysis.
-                  </Button>
-                </div>
-              </Modal.Actions>
-            </Modal>
-            );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer transition={ Zoom } />
+        <Modal dimmer={ "blurring" } trigger={ <ClearProjectButton onClick={ handleOpen } size="large">
+                                                 Clear Project
+                                               </ClearProjectButton> } open={ localStore.modalOpen } className="wrapper1" onClose={ this.handleClose } basic size={ "small" }>
+          <Header content="Clear Project" />
+          <Modal.Content>
+            <h2>
+                                                      This will remove all data and analysis, and cannot be reversed.
+                                                    </h2>
+            <h2> Are you sure you want to clear the current project?</h2>
+          </Modal.Content>
+          <Modal.Actions>
+            <div style={ { display: "flex" } }>
+              <Button size={ "big" } style={ { alignSelf: "flexStart" } } color="green" onClick={ handleClose } inverted>
+                No, Go back.
+              </Button>
+              <Button id="resetAnalysisModalGotItButton" size={ "big" } style={ { alignSelf: "flexEnd", marginLeft: 220 } } color="red" onClick={ clearAnalysis } inverted>
+                Yes, delete the
+                <br /> data and analysis.
+              </Button>
+            </div>
+          </Modal.Actions>
+        </Modal>
+      </React.Fragment>
+      );
+  }
 }
 
 export default view(ClearProjectModal);
@@ -80,3 +94,4 @@ const ClearProjectButton = styled.button`
     transform: translateY(1px);
   }
 `;
+
