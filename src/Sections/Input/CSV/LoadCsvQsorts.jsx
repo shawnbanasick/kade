79fb457bf2620caf541ccf9d2492da71e5fb1840
Shawn Banasick <banasick@mail.kobe-c.ac.jp>
2019-02-23 08:15:02 +0900

    added no q sort error check in csv

--- src/Sections/Input/CSV/LoadCsvQsorts.jsx
+++ src/Sections/Input/CSV/LoadCsvQsorts.jsx
@@ -4,158 +4,173 @@ import React, { Component } from "react";
 import { view, store } from "react-easy-state";
 import state from "../../../store";
 import sortsDisplayText from "../logic/sortsDisplayText";
+import throwNoSortsInputError from "../throwNoSortsInputError";
 import shiftRawSortsPositive from "../logic/shiftRawSortsPositive";
+import revertLoadButtonsColors from "../DemoData/revertLoadButtonsColors";
 import calcMultiplierArrayT2 from "../Excel/excelLogic/calcMultiplierArrayT2";
 import checkUniqueParticipantNames from "../logic/checkUniqueParticipantName";
-import revertLoadButtonsColors from '../DemoData/revertLoadButtonsColors';
 import throwDataAlreadyLoadedInputErrorModal from "../throwDataAlreadyLoadedInputErrorModal";
 
-const {dialog} = require("electron").remote;
+const { dialog } = require("electron").remote;
 const fs = require("fs");
 
+let hasInputError = false;
+
 const localStore = store({
-    isLoadCsvQsortsButtonGreen: false,
+  isLoadCsvQsortsButtonGreen: false
 });
 
 const handleClick = () => {
-    // check to see if data loaded and correlations started - true ==> throw error 
-    const isDataAlreadyLoaded = state.getState("isDataAlreadyLoaded");
-    if (isDataAlreadyLoaded) {
-        throwDataAlreadyLoadedInputErrorModal();
-    } else {
-        try {
-            dialog.showOpenDialog(
-                {
-                    properties: ["openFile"],
-                    filters: [
-                        {
-                            name: "CSV",
-                            extensions: ["csv", "CSV"]
-                        }
-                    ]
-                },
-                files => {
-                    try {
-                        if (files !== undefined) {
-                            const fileName = files[0];
-                            fs.readFile(fileName, "utf-8", (error, data) => {
-                                // parse file
-                                const parsedFile = Papa.parse(data);
-                                const lines2 = parsedFile.data;
-                                let qSortPatternArray;
+  // check to see if data loaded and correlations started - true ==> throw error
+  const isDataAlreadyLoaded = state.getState("isDataAlreadyLoaded");
+  if (isDataAlreadyLoaded) {
+    throwDataAlreadyLoadedInputErrorModal();
+  } else {
+    try {
+      dialog.showOpenDialog(
+        {
+          properties: ["openFile"],
+          filters: [
+            {
+              name: "CSV",
+              extensions: ["csv", "CSV"]
+            }
+          ]
+        },
+        files => {
+          try {
+            if (files !== undefined) {
+              const fileName = files[0];
+              fs.readFile(fileName, "utf-8", (error, data) => {
+                // parse file
+                const parsedFile = Papa.parse(data);
+                const lines2 = parsedFile.data;
+                let qSortPatternArray;
 
-                                // remove the first (header) line
-                                lines2.shift();
+                // remove the first (header) line
+                lines2.shift();
 
-                                // parsing first line of PQMethod file to set qav variables
-                                const numberSorts = lines2.length;
-                                if (lines2.length < 2) {
-                                    throw new Error("Can't find any Q sorts in the file!");
-                                }
-
-                                // remove empty "" strings from array
-                                let maxLength = lines2[0].length;
-                                for (let i = 0; i < lines2[0].length - 1; i += 1) {
-                                    const value1 = lines2[0][i];
-                                    if (value1 === "") {
-                                        maxLength = i;
-                                        break;
-                                    }
-                                }
-
-                                // todo - check if other data import methods check to see if min value is above zero
-                                // before doing positive shift for raw sorts
-                                let minValue;
-                                let arrayShiftedPositive;
-                                const mainDataObject = [];
-                                const respondentNames = [];
-                                for (let j = 0; j < lines2.length; j += 1) {
-                                    lines2[j].length = maxLength;
-                                    const tempObj = {};
-                                    // get name
-                                    const name = lines2[j].shift();
-                                    // slice off name
-                                    lines2[j] = lines2[j].slice(1, -1);
-                                    tempObj.name = name;
-                                    respondentNames.push(name);
-                                    const asNumbers = lines2[j].map(Number);
-                                    if (j === 0) {
-                                        minValue = Math.min(...asNumbers);
-                                    }
-                                    // grab last for for qSortPattern
-                                    qSortPatternArray = asNumbers;
-
-                                    if (minValue < 1) {
-                                        arrayShiftedPositive = shiftRawSortsPositive(
-                                            asNumbers,
-                                            minValue
-                                        );
-                                    } else {
-                                        arrayShiftedPositive = [...asNumbers];
-                                    }
-                                    tempObj.posShiftSort = arrayShiftedPositive;
-                                    tempObj.rawSort = asNumbers;
-                                    tempObj.displaySort = lines2[j].toString();
-                                    mainDataObject.push(tempObj);
-                                }
-
-                                qSortPatternArray.sort((a, b) => a - b);
-
-                                const multiplierArray = calcMultiplierArrayT2([
-                                    ...qSortPatternArray
-                                ]);
-
-                                const sortsDisplayTextArray = sortsDisplayText(mainDataObject);
-
-                                const participantNames = checkUniqueParticipantNames(
-                                    respondentNames
-                                );
-                                revertLoadButtonsColors("csv");
-                                state.setState({
-                                    numQsorts: numberSorts,
-                                    qSortPattern: qSortPatternArray,
-                                    numStatements: lines2[0].length,
-                                    respondentNames: participantNames,
-                                    mainDataObject,
-                                    sortsDisplayText: sortsDisplayTextArray,
-                                    multiplierArray,
-                                    dataOrigin: "csv",
-                                    sortsLoaded: true,
-                                    notifyDataUploadSuccess: true,
-                                    areQsortsLoaded: true,
-                                    isInputButtonGreen: state.getState("areStatementsLoaded"),
-                                    isLoadCsvQsortsButtonGreen: true,
-                                });
-                                localStore.isLoadCsvQsortsButtonGreen = true;
-                            });
-                        }
-
-                    } catch (error) {
-                        // console.log("error");
-                    }
+                // parsing first line of PQMethod file to set qav variables
+                const numberSorts = lines2.length;
+                if (lines2.length < 2) {
+                  throw new Error("Can't find any Q sorts in the file!");
                 }
 
-            );
-        } catch (error) {
-            // console.log(JSON.stringify("catch called"));
-            state.setState({
-                errorMessage: error.message,
-                showErrorMessageBar: true
-            });
+                // remove empty "" strings from array
+                let maxLength = lines2[0].length;
+                for (let i = 0; i < lines2[0].length - 1; i += 1) {
+                  const value1 = lines2[0][i];
+                  if (value1 === "") {
+                    maxLength = i;
+                    break;
+                  }
+                }
+
+                // check for empty file
+                if (lines2.length === 0) {
+                  throwNoSortsInputError();
+                  hasInputError = true;
+                }
+
+                // todo - check if other data import methods check to see if min value is above zero
+                // before doing positive shift for raw sorts
+                let minValue;
+                let arrayShiftedPositive;
+                const mainDataObject = [];
+                const respondentNames = [];
+                for (let j = 0; j < lines2.length; j += 1) {
+                  lines2[j].length = maxLength;
+                  const tempObj = {};
+                  // get name
+                  const name = lines2[j].shift();
+                  // slice off name
+                  lines2[j] = lines2[j].slice(1, -1);
+                  tempObj.name = name;
+                  respondentNames.push(name);
+                  const asNumbers = lines2[j].map(Number);
+                  if (j === 0) {
+                    minValue = Math.min(...asNumbers);
+                  }
+                  // grab last for for qSortPattern
+                  qSortPatternArray = asNumbers;
+
+                  if (minValue < 1) {
+                    arrayShiftedPositive = shiftRawSortsPositive(
+                      asNumbers,
+                      minValue
+                    );
+                  } else {
+                    arrayShiftedPositive = [...asNumbers];
+                  }
+                  tempObj.posShiftSort = arrayShiftedPositive;
+                  tempObj.rawSort = asNumbers;
+                  tempObj.displaySort = lines2[j].toString();
+                  mainDataObject.push(tempObj);
+                }
+
+                qSortPatternArray.sort((a, b) => a - b);
+
+                const multiplierArray = calcMultiplierArrayT2([
+                  ...qSortPatternArray
+                ]);
+
+                const sortsDisplayTextArray = sortsDisplayText(mainDataObject);
+
+                const participantNames = checkUniqueParticipantNames(
+                  respondentNames
+                );
+
+                if (hasInputError === false) {
+                  revertLoadButtonsColors("csv");
+                  state.setState({
+                    numQsorts: numberSorts,
+                    qSortPattern: qSortPatternArray,
+                    numStatements: lines2[0].length,
+                    respondentNames: participantNames,
+                    mainDataObject,
+                    sortsDisplayText: sortsDisplayTextArray,
+                    multiplierArray,
+                    dataOrigin: "csv",
+                    sortsLoaded: true,
+                    notifyDataUploadSuccess: true,
+                    areQsortsLoaded: true,
+                    isInputButtonGreen: state.getState("areStatementsLoaded"),
+                    isLoadCsvQsortsButtonGreen: true
+                  });
+                  localStore.isLoadCsvQsortsButtonGreen = true;
+                }
+              });
+            }
+          } catch (error) {
+            // console.log("error");
+          }
         }
+      );
+    } catch (error) {
+      // console.log(JSON.stringify("catch called"));
+      state.setState({
+        errorMessage: error.message,
+        showErrorMessageBar: true
+      });
     }
+  }
 };
 
 class LoadTxtStatementFile extends Component {
-    render() {
-        const isLoadCsvQsortsButtonGreen = state.getState("isLoadCsvQsortsButtonGreen");
-        localStore.isLoadCsvQsortsButtonGreen = isLoadCsvQsortsButtonGreen;
-        return (
-            <LoadTxtButton isActive={ localStore.isLoadCsvQsortsButtonGreen } onClick={ handleClick }>
-              <p>Load CSV File</p>
-            </LoadTxtButton>
-            );
-    }
+  render() {
+    const isLoadCsvQsortsButtonGreen = state.getState(
+      "isLoadCsvQsortsButtonGreen"
+    );
+    localStore.isLoadCsvQsortsButtonGreen = isLoadCsvQsortsButtonGreen;
+    return (
+      <LoadTxtButton
+        isActive={localStore.isLoadCsvQsortsButtonGreen}
+        onClick={handleClick}
+      >
+        <p>Load CSV File</p>
+      </LoadTxtButton>
+    );
+  }
 }
 
 export default view(LoadTxtStatementFile);
@@ -164,7 +179,8 @@ const LoadTxtButton = styled.button`
   display: grid;
   align-items: center;
   justify-items: center;
-  background-color: ${props => props.isActive ? "rgba(144,	238, 144, .6)" : "#d6dbe0"};
+  background-color: ${props =>
+    props.isActive ? "rgba(144,	238, 144, .6)" : "#d6dbe0"};
   height: 60px;
   width: 240px;
   border: 1px solid black;
@@ -179,7 +195,7 @@ const LoadTxtButton = styled.button`
   outline: none;
 
   &:hover {
-    background-color: ${props => props.isActive ? "#009a00" : "#abafb3" };
+    background-color: ${props => (props.isActive ? "#009a00" : "#abafb3")};
   }
 
   &:active {
