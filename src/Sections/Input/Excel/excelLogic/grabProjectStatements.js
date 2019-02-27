b41a605236dcdfb016fb52d0cfaff4a43cfca377
Shawn Banasick <banasick@mail.kobe-c.ac.jp>
2019-02-27 18:10:18 +0900

    minor boyscouting

--- src/Sections/Input/Excel/excelLogic/grabProjectStatements.js
+++ src/Sections/Input/Excel/excelLogic/grabProjectStatements.js
@@ -1,5 +1,4 @@
 function grabProjectStatements(statementsDataT2) {
-  console.log('TCL: grabProjectStatements -> statementsDataT2', JSON.stringify(statementsDataT2));
   const qavCurrentStatements = [];
   for (let s = 0, sLen = statementsDataT2.length; s < sLen; s += 1) {
     const temp12 = statementsDataT2[s].Statements;
