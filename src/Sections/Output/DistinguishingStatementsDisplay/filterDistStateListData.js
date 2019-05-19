9a25e033d9086329c006892b82bbd66fbd8d580c
Mac15 <banasick@mail.kobe-c.ac.jp>
2019-05-20 08:55:12 +0900

    fixed sort by buttons in dist states list

--- src/Sections/Output/DistinguishingStatementsDisplay/filterDistStateListData.js
+++ src/Sections/Output/DistinguishingStatementsDisplay/filterDistStateListData.js
@@ -1,29 +1,57 @@
-import state from '../../../store';
+import state from "../../../store";
 
 const filterDistStateListData = (thresholdLevel, sortKey) => {
-    const distStateListData = state.getState("distStateListData");
-    for (let i = 0; i< distStateListData.length; i++) {
-        distStateListData[i]["distStates"] = distStateListData[i]["distStates"].filter(item => item.sigLevelRank >= thresholdLevel);
-    }
-    
-    if (sortKey === "threshold") {
-        for (let i=0; i<distStateListData.length; i++) {
-            distStateListData[i]["distStates"].sort((a, b) => {
-              if (a.sigLevelRank === b.sigLevelRank) {
-                return a.factorNum - b.factorNum;
-              } else if (a.sigLevelRank > b.sigLevelRank) {
-                return -1;
-              } else if (a.sigLevelRank < b.sigLevelRank) {
-                return 1;
-              }
-            });
+  const distStateListData = state.getState("distStateListData");
+  for (let i = 0; i < distStateListData.length; i++) {
+    distStateListData[i].distStates = distStateListData[i].distStates.filter(
+      item => item.sigLevelRank >= thresholdLevel
+    );
+  }
+
+  if (sortKey === "threshold") {
+    for (let i = 0; i < distStateListData.length; i++) {
+      distStateListData[i].distStates.sort((a, b) => {
+        if (a.sigLevelRank === b.sigLevelRank) {
+          return a.factorNum - b.factorNum;
+        } else if (a.sigLevelRank > b.sigLevelRank) {
+          return -1;
+        } else if (a.sigLevelRank < b.sigLevelRank) {
+          return 1;
         }
+      });
     }
+  }
 
-    return distStateListData;
+  if (sortKey === "qSortValue") {
+    for (let i = 0; i < distStateListData.length; i++) {
+      distStateListData[i].distStates.sort((a, b) => {
+        if (a.sortValue === b.sortValue) {
+          return a.sigLevelRank - b.sigLevelRank;
+        } else if (a.sortValue > b.sortValue) {
+          return 1;
+        } else if (a.sortValue < b.sortValue) {
+          return -1;
+        }
+      });
+    }
+  }
 
+  if (sortKey === "statementNum") {
+    for (let i = 0; i < distStateListData.length; i++) {
+      distStateListData[i].distStates.sort((a, b) => {
+        if (a.statement === b.statement) {
+          return a.sigLevelRank - b.sigLevelRank;
+        } else if (a.statement > b.statement) {
+          return 1;
+        } else if (a.statement < b.statement) {
+          return -1;
+        }
+      });
+    }
+  }
 
-}
+  return distStateListData;
+};
 
 export default filterDistStateListData;
 
