900af422180529e5d1ab362ecbd67de1e0f14079
banasick <banasick@mail.kobe-c.ac.jp>
2024-08-03 19:21:07 +0900

    composite factor image downloads working

--- src/renderer/src/Sections/Output/FactorVisualizations/DownloadFactorVizButtons.jsx
+++ src/renderer/src/Sections/Output/FactorVisualizations/DownloadFactorVizButtons.jsx
@@ -1,4 +1,3 @@
-import { saveSvgAsPng } from 'save-svg-as-png';
 import currentDate from '../../../Utils/currentDate1';
 import currentTime from '../../../Utils/currentTime1';
 import GeneralButton from '../../../Utils/GeneralButton';
@@ -8,105 +7,125 @@ import PngIcon from '../../images/PNG_Icon2.svg';
 import styled from 'styled-components';
 import vizState from '../../GlobalState/vizState';
 import coreState from '../../GlobalState/coreState';
+import d3ToPng from 'd3-svg-to-png';
 
-const downloadSvgImage = (imageId) => {
+const DownloadFactorVizButtons = (props) => {
+  const { t } = useTranslation();
   const factorVizOptions = vizState((state) => state.factorVizOptions);
-
-  const shouldAddName = factorVizOptions.willAddCustomNameToDownload;
-  const imageName = `#image${imageId}`;
-
   const projectName = coreState((state) => state.projectName);
 
-  const date = currentDate();
-  const time = currentTime();
-  const dateTime = `${date}_${time}`;
-  const cleanFactorName = `${imageId}`;
-  let config;
-  const customName = factorVizOptions.customDownloadFileNames;
-  const customNameLocation = factorVizOptions.customFileNameLocation;
-  if (shouldAddName === true) {
-    if (customNameLocation === 'prepend') {
-      config = {
-        filename: `${customName}_${projectName}_${cleanFactorName}_${dateTime}`,
-      };
-    } else if (customNameLocation === 'append') {
-      config = {
-        filename: `${projectName}_${cleanFactorName}_${dateTime}_${customName}`,
-      };
-    } else if (customNameLocation === 'replace') {
-      config = {
-        filename: customName,
-      };
+  const downloadSvgImage = (imageId) => {
+    const shouldAddName = factorVizOptions.willAddCustomNameToDownload;
+    const imageName = `#image${imageId}`;
+    const date = currentDate();
+    const time = currentTime();
+    const dateTime = `${date}_${time}`;
+    const cleanFactorName = `${imageId}`;
+    let config;
+    const customName = factorVizOptions.customDownloadFileNames;
+    const customNameLocation = factorVizOptions.customFileNameLocation;
+    if (shouldAddName === true) {
+      if (customNameLocation === 'prepend') {
+        config = {
+          filename: `${customName}_${projectName}_${cleanFactorName}_${dateTime}`,
+        };
+      } else if (customNameLocation === 'append') {
+        config = {
+          filename: `${projectName}_${cleanFactorName}_${dateTime}_${customName}`,
+        };
+      } else if (customNameLocation === 'replace') {
+        config = {
+          filename: customName,
+        };
+      } else {
+        config = {
+          filename: `${projectName}_${cleanFactorName}_${dateTime}`,
+        };
+      }
     } else {
       config = {
         filename: `${projectName}_${cleanFactorName}_${dateTime}`,
       };
     }
-  } else {
-    config = {
-      filename: `${projectName}_${cleanFactorName}_${dateTime}`,
+
+    const svg = document.querySelector(imageName);
+
+    const saveSvg = async (svgEl, name) => {
+      svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+      const svgData = svgEl.outerHTML;
+      const preface = '<?xml version="1.0" standalone="no"?>\r\n';
+      const svgBlob = new Blob([preface, svgData], {
+        type: 'image/svg+xml;charset=utf-8',
+      });
+
+      // to buffer
+      const arrayBuffer = await new Response(svgBlob).arrayBuffer();
+      const defaultPath = `${name}.svg`;
+
+      const filepath = await window.electronAPI.showSaveSvgDialog(defaultPath);
+      if (!filepath) {
+        alert('Save operation was canceled.');
+        return;
+      }
+
+      try {
+        const result = await window.electronAPI.saveSVG(arrayBuffer, filepath);
+        console.log(result);
+      } catch (error) {
+        console.error('Failed to save file:', error);
+      }
     };
-  }
+    saveSvg(svg, config.filename);
+  };
 
-  const svg = document.querySelector(imageName);
+  const downloadFacVizAsPng = (imageId) => {
+    const shouldAddName = factorVizOptions.willAddCustomNameToDownload;
+    const imageName = `image${imageId}`;
+    const date = currentDate();
+    const time = currentTime();
+    const dateTime = `${date}__${time}`;
+    const customName = factorVizOptions.customDownloadFileNames;
+    const customNameLocation = factorVizOptions.customFileNameLocation;
+    const cleanFactorName = `${imageId}__`;
+    let nameConfig;
 
-  // https://stackoverflow.com/questions/23218174/how-do-i-save-export-an-svg-file-after-
-  // creating-an-svg-with-d3-js-ie-safari-an
-  function saveSvg(svgEl, name) {
-    svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    const svgData = svgEl.outerHTML;
-    const preface = '<?xml version="1.0" standalone="no"?>\r\n';
-    const svgBlob = new Blob([preface, svgData], {
-      type: 'image/svg+xml;charset=utf-8',
-    });
-    const svgUrl = URL.createObjectURL(svgBlob);
-    const downloadLink = document.createElement('a');
-    downloadLink.href = svgUrl;
-    downloadLink.download = name;
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-  }
-  saveSvg(svg, config.filename);
-};
-
-const downloadFacVizAsPng = (imageId) => {
-  // getState
-  const factorVizOptions = vizState((state) => state.factorVizOptions);
-  const shouldAddName = factorVizOptions.willAddCustomNameToDownload;
-
-  const imageName = `image${imageId}`;
-
-  const projectName = coreState((state) => state.projectName);
-
-  const date = currentDate();
-  const time = currentTime();
-  const dateTime = `${date}__${time}`;
-  const customName = factorVizOptions.customDownloadFileNames;
-  const customNameLocation = factorVizOptions.customFileNameLocation;
-  const cleanFactorName = `${imageId}__`;
-  let nameConfig;
-
-  if (shouldAddName === true) {
-    if (customNameLocation === 'prepend') {
-      nameConfig = `${customName}_${projectName}_${cleanFactorName}${dateTime}`;
-    } else if (customNameLocation === 'append') {
-      nameConfig = `${projectName}__${cleanFactorName}${dateTime}_${customName}`;
-    } else if (customNameLocation === 'replace') {
-      nameConfig = customName;
+    if (shouldAddName === true) {
+      if (customNameLocation === 'prepend') {
+        nameConfig = `${customName}_${projectName}_${cleanFactorName}${dateTime}`;
+      } else if (customNameLocation === 'append') {
+        nameConfig = `${projectName}__${cleanFactorName}${dateTime}_${customName}`;
+      } else if (customNameLocation === 'replace') {
+        nameConfig = customName;
+      } else {
+        nameConfig = `${projectName}__${cleanFactorName}${dateTime}`;
+      }
     } else {
       nameConfig = `${projectName}__${cleanFactorName}${dateTime}`;
     }
-  } else {
-    nameConfig = `${projectName}__${cleanFactorName}${dateTime}`;
-  }
-  saveSvgAsPng(document.getElementById(imageName), nameConfig, {
-    encoderOptions: 1,
-  });
-};
 
-const DownloadFactorVizButtons = (props) => {
-  const { t } = useTranslation();
+    d3ToPng(document.getElementById(imageName), nameConfig, {
+      backgroundColor: 'white',
+      scale: 3,
+      format: 'png',
+      download: false,
+      quality: 1,
+    }).then(async (fileData) => {
+      const buffer = fileData.split(',')[1];
+      const defaultPath = `${nameConfig}.png`;
+      const filepath = await window.electronAPI.showSavePngDialog(defaultPath);
+      if (!filepath) {
+        alert('Save operation was canceled.');
+        return;
+      }
+
+      try {
+        const result = await window.electronAPI.savePNG(buffer, filepath);
+        console.log(result);
+      } catch (error) {
+        console.error('Failed to save file:', error);
+      }
+    });
+  };
 
   return (
     <div style={{ display: 'flex' }}>
