5c72a74d187005d68d7eaa00b4a304739069e97f
banasick <banasick@mail.kobe-c.ac.jp>
2024-08-07 16:17:42 +0900

    web worker development

--- src/renderer/src/Sections/Structure/webWorkerPca.js
+++ src/renderer/src/Sections/Structure/webWorkerPca.js
@@ -1,27 +1,24 @@
 import registerPromiseWorker from 'promise-worker/register';
-// import determineNumberPCs from '../Factors/PcaLogic/determineNumberPCs';
 import getSvd from '../Factors/PcaLogic/svd';
 
 import sortEigenValues from '../Factors/PcaLogic/sortEigenValues';
 import calcEigenVectors from '../Factors/PcaLogic/calcEigenVectors';
 import transposeMatrix from '../../Utils/transposeMatrix';
 import inflectPrincipalComponents from '../Factors/PcaLogic/inflectPrincipalComponents';
+import calcSumSquares from '../Rotation/varimaxLogic/2calcSumSquares';
+import calcStandardizedFactorMatrix from '../Rotation/varimaxLogic/2calcStandardizedFactorMatrix';
+import doVarimaxRotations from '../Rotation/varimaxLogic/2doVarimaxRotations';
 // import calculateCommunalities from '../Rotation/varimaxLogic/2calculateCommunalities';
 
 registerPromiseWorker(function (array) {
-  // [X, numberofPrincipalComps]
-  //   console.log('message', message);
-  //   return 'pong';
+  // array contents are stringified [X, numberofPrincipalComps]
 
   const array2 = JSON.parse(array);
-
   const X = array2[0];
   const numberofPrincipalComps = array2[1];
 
   const m = X.length;
-
   const numberOfSorts = m;
-  //   console.log('X', X);
   const svdResults = getSvd(X);
 
   // calcualte svd from correlations
@@ -30,7 +27,6 @@ registerPromiseWorker(function (array) {
   const eigenValuesSorted = sortEigenValues(eigens);
 
   //   const getEigenCumulPercentArray = calcEigenCumulPercentArray(eigenValuesSorted, m);
-
   //   const eigenValuesAsPercents = getEigenCumulPercentArray[0];
   //   const eigenValuesCumulPercentArray = getEigenCumulPercentArray[1];
 
@@ -47,8 +43,16 @@ registerPromiseWorker(function (array) {
 
   //   calculateCommunalities([...eigenVecs]);
 
-  // transpose
-  const eigenVecsTransposed = transposeMatrix(eigenVecs);
+  // transpose to unroatated principal components
+  let unrotatedComponents = transposeMatrix(eigenVecs);
 
-  return eigenVecsTransposed;
+  unrotatedComponents.length = numberofPrincipalComps;
+
+  const sumSquares = calcSumSquares(unrotatedComponents);
+  const standardizedFactorMatrix = calcStandardizedFactorMatrix(sumSquares, unrotatedComponents);
+
+  const rotatedResults = doVarimaxRotations(standardizedFactorMatrix, sumSquares);
+  const transposedRotatedResults = transposeMatrix(rotatedResults);
+
+  return transposedRotatedResults;
 });
