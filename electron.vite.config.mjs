a9b2944f8b44286d81b18802748e045bda8ed59c
banasick <banasick@mail.kobe-c.ac.jp>
2026-02-06 21:21:27 +0900

    fixed ag-grid update issue

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -10,6 +10,48 @@ export default defineConfig({
   preload: {
     plugins: [externalizeDepsPlugin()],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // 1. Core Dependencies (React, ReactDOM, Redux)
+          'react-core': ['react', 'react-dom', 'react-redux', 'redux', 'redux-thunk'],
+
+          // 2. UI Libraries (MUI, Emotion)
+          'ui-libraries': [
+            '@mui/material',
+            '@mui/icons-material',
+            '@emotion/react',
+            '@emotion/styled',
+          ],
+
+          // 3. Charting (Recharts)
+          charting: ['recharts'],
+
+          // 4. Data Grid (AG Grid)
+          'data-grid': ['ag-grid-community', 'ag-grid-react'],
+
+          // 5. Utility Libraries
+          utilities: [
+            'axios',
+            'lodash',
+            'file-saver',
+            'docx',
+            'xlsx',
+            'jspdf',
+            'html2canvas',
+            'promise-worker/register',
+          ],
+
+          // 6. Routing & Navigation
+          routing: ['react-router-dom'],
+
+          // 7. i18n
+          i18n: ['i18next', 'react-i18next'],
+        },
+      },
+    },
+  },
   renderer: {
     resolve: {
       alias: {
